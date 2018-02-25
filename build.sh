e0a7c901d48ed3ac74f5796b90996789d8d2c02f
Kirill Grosul <kirill.grosul@gmail.com>
2018-02-25 22:56:14 +0300

    change master

--- build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-g++ -std=c++11 main.cpp run.cpp makepizza.cpp otherfood.cpp pizza.cpp -lgtest -pthread -lgtest_main -L/usr/lib/
--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "run.h"
-#include "gtest/gtest.h"
-#include "tests.cpp"
-
-int main(int argc, char **argv) {
-    testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
-    //run(); - use to start
-}
